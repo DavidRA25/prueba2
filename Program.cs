1ad47a6dde51bba43bc4b96ce7254aba9637bdee
DavidRA25 <aguilardavidrodriguez@gmail.com>
2021-05-13 17:50:20 +0200

    he añadido codigo for

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace prueba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100;
+            for (int i = 0; i<x; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
